40d0b8bcdade58bac5ed3ed5488ba5ae3b4e4c00
Anna Trzeciak <trzeciakanna@hotmail.com>
2020-10-24 18:37:23 +0200

    feat: added search page and used auth middleware

---
+++ pages/search.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="page container">
+    <h1 class="page__title main-page">Search projects</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Search",
+  middleware: "auth",
+  mounted() {
+    // remove queries which were set during redirect after auth
+    if (this.$route.query) this.$router.push({ query: {} })
+  },
+}
+</script>
+
+<style scoped></style>
